4e4c61725958abdb8bcf0880e79a8b9c59b226fd
Ayoub Gharbi <ayoub@truluv.ai>
2022-11-06 19:00:52 -0500

    Dash logic

--- Assets/Characters/Player/Movement/PlayerMovement.cs
+++ Assets/Characters/Player/Movement/PlayerMovement.cs
@@ -1,54 +1,71 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class PlayerMovement : MonoBehaviour
+namespace Characters.Player.Movement
 {
-    [field: Range(1, 100)]
-    [field: SerializeField]
-    public float PlayerSpeed { get; private set; }
-
-    private Inputs _inputs;
-    private Vector2 _playerDirection;
-    private Rigidbody2D _playerRidigbody;
-
-    private void Awake()
-        => Init();
-
-    private void OnEnable()
-        => InitCallbacks(true);
-
-    private void OnDisable()
-        => InitCallbacks(false);
-
-    private void Init()
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerMovement : MonoBehaviour
     {
-        _inputs = new Inputs();
-        TryGetComponent(out _playerRidigbody);
-    }
+        [field: Range(1, 1000)]
+        [field: SerializeField]
+        public float PlayerSpeed { get; private set; }
 
-    private void InitCallbacks(bool enable)
-    {
-        if (enable)
+        private Inputs _inputs;
+        private Vector2 _playerDirection;
+        private PlayerDash _playerDash;
+        private Rigidbody2D _playerRidigbody;
+
+        private void Awake()
+            => Init();
+
+        private void OnEnable()
+            => InitCallbacks(true);
+
+        private void OnDisable()
+            => InitCallbacks(false);
+
+        private void Init()
         {
-            _inputs.Enable();
-            _inputs.Player.Move.performed += OnMovement;
+            _inputs = new Inputs();
+            TryGetComponent(out _playerDash);
+            TryGetComponent(out _playerRidigbody);
         }
-        else
+
+        private void InitCallbacks(bool enable)
         {
-            _inputs.Disable();
-            _inputs.Player.Move.performed -= OnMovement;
+            if (enable)
+            {
+                _inputs.Enable();
+                _inputs.Player.Move.performed += OnMovement;
+            }
+            else
+            {
+                _inputs.Disable();
+                _inputs.Player.Move.performed -= OnMovement;
+            }
         }
-    }
 
-    private void OnMovement(InputAction.CallbackContext movementCxt)
-    {
-        _playerDirection = movementCxt.ReadValue<Vector2>();
-    }
+        private void OnMovement(InputAction.CallbackContext movementCxt)
+        {
+            _playerDirection = movementCxt.ReadValue<Vector2>();
+        }
 
-    private void FixedUpdate()
-    {
-        var targetPosition = _playerRidigbody.position + _playerDirection * (PlayerSpeed * Time.fixedDeltaTime);
-        _playerRidigbody.MovePosition(targetPosition);
+        private void FixedUpdate()
+        {
+            if (_playerDash.CanDash)
+            {
+                _playerDash.Dash();
+            }
+            else
+            {
+                Move();
+            }
+        }
+
+        private void Move()
+        {
+            var targetPosition = _playerRidigbody.position + _playerDirection * (PlayerSpeed * Time.fixedDeltaTime);
+            _playerRidigbody.MovePosition(targetPosition);
+        }
     }
 }
