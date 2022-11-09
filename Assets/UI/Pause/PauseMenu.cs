d0775a412cbca402e57f3950f890cfaac13b45e7
Ayoub Gharbi <ayoub@truluv.ai>
2022-11-09 18:26:49 -0500

    Pause menu with Escape key.

---
+++ Assets/UI/Pause/PauseMenu.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace UI.Pause
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        [SerializeField] private GameObject _pauseContainer;
+
+        private Inputs _inputs;
+        private bool _lastPauseState = false;
+
+        private void Awake()
+        {
+            _inputs = new Inputs();
+        }
+
+        private void OnEnable()
+        {
+            _inputs.Enable();
+            _inputs.Player.Pause.performed += cxt => UpdatePauseMenu(!_lastPauseState);
+        }
+
+        private void OnDisable()
+        {
+            _inputs.Disable();
+            _inputs.Player.Pause.performed -= cxt => UpdatePauseMenu(!_lastPauseState);
+        }
+
+        public void UpdatePauseMenu(bool show)
+        {
+            Debug.Log("Hello");
+            _lastPauseState = show;
+            Time.timeScale = show ? 0f : 1f;
+            _pauseContainer.SetActive(show);
+        }
+    }
+}
