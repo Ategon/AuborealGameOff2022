098dc33fd2b8141547888cbd9ed52ac3d1bc2b0c
NnYGames <NnYMakingGames@gmail.com>
2022-11-15 00:47:38 +0200

    .

---
+++ Assets/UI/GameManager/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject LoseScreen;
+    [SerializeField] private GameObject WinScreen;
+    public void Lose()
+    {
+        LoseScreen.SetActive(true);
+    }
+    
+    public void Win()
+    {
+        WinScreen.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
