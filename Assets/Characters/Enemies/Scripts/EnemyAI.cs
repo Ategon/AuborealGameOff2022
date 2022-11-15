273baf0ee222c97ad3ed4b3b5d504e2b73659a0e
Leafcutter <alex.m.covington@gmail.com>
2022-11-15 14:36:23 -0500

    Added ranged enemy implementation

--- Assets/Characters/Enemies/Scripts/EnemyAI.cs
+++ Assets/Characters/Enemies/Scripts/EnemyAI.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class EnemyAI : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class EnemyAI : MonoBehaviour
     Animator anim;
     public Transform player;
     public MeleeAttack meleeAttack;
+    public ShootProjectile rangedAttack;
     private StateMathematicalValues stateMathValues;
     public List<GameObject> checkpoints;
 
@@ -26,16 +28,36 @@ public class EnemyAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        stateMathValues = new StateMathematicalValues(visDist, attackDist, patrolDist, attackCooldown, patrolSpeed,
+        int projectileCollisionLayers = 0;
+        if (rangedAttack != null)
+        {
+            Projectile projectile = rangedAttack.projectilePrefab.GetComponent<Projectile>();
+            int projectileLayer = projectile.gameObject.layer;
+            LayerMask targetLayers = projectile.targetLayers;
+            for (int i = 0; i < 32; i++)
+            {
+                if (!Physics2D.GetIgnoreLayerCollision(projectileLayer, i))
+                {
+                    if (!((targetLayers.value & 1 << i) == 1 << i))
+                    {
+                        projectileCollisionLayers |= 1 << i;
+
+                    }
+                }
+            }
+        }
+        Debug.Log(projectileCollisionLayers);
+        stateMathValues = new StateMathematicalValues(visDist, attackDist, projectileCollisionLayers, patrolDist, attackCooldown, patrolSpeed,
             pursueSpeed, checkpoints, isMeleeAttacking, isRangedAttacking);
         ;
         agent = this.GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
         anim = this.GetComponent<Animator>();
-        StateValues stateValues = new StateValues(this.gameObject, agent, anim, player, meleeAttack, stateMathValues);
-        ;
+        StateValues stateValues = new StateValues(this.gameObject, agent, anim, player, meleeAttack, rangedAttack, stateMathValues);
         currentState = new Idle(stateValues);
+
+
     }
 
     // Update is called once per frame
