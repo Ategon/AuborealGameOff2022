c87b84428f5f8046c03acb4e23b5a49f6a76c514
Benjamin Barbeau <benjamin@barbeau.net>
2022-11-19 03:31:47 -0500

    Add smooth health bar

--- Assets/Characters/Player/Health/HealthMeter.cs
+++ Assets/Characters/Player/Health/HealthMeter.cs
@@ -2,6 +2,8 @@ using Assets.EventSystem;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections;
+using DG.Tweening;
 
 namespace Assets.Player.Health
 {
@@ -10,7 +12,13 @@ namespace Assets.Player.Health
         [SerializeField] private HealthChangedEvent healthChangedEvent;
         [SerializeField] private HealthController healthController;
         [SerializeField] private Image healthFill;
+        [SerializeField] private Image healthDamageFill;
+        [SerializeField] private Image healthHealedFill;
         [SerializeField] private TextMeshProUGUI healthText;
+
+        private Tween lastTween = null;
+        private Tween lastSecondaryTween = null;
+
         private void Start()
         {
             UpdateMeter();
@@ -23,12 +31,36 @@ namespace Assets.Player.Health
         {
             healthChangedEvent.RemoveListener(OnHealthChange);
         }
-
+        
         private void UpdateMeter()
         {
-            healthFill.fillAmount = ((float) healthController.currentHealth) / healthController.maxHealth;
-            healthText.text = "Health: " + healthController.currentHealth + " / " + healthController.maxHealth;
+            if (lastTween != null)
+            {
+                lastTween.Pause();
+                lastSecondaryTween.Pause();
+            }
+
+            float start = healthFill.fillAmount;
+            float end = (float)healthController.currentHealth / (float)healthController.maxHealth;
+
+            if (start > end)
+            {
+                healthDamageFill.fillAmount = start;
+                healthHealedFill.fillAmount = end;
+                healthFill.fillAmount = end;
+                lastSecondaryTween = DOTween.To(() => healthFill.fillAmount, x => healthFill.fillAmount = x, end, 0.15f).SetEase(Ease.InOutQuad, 0.1f);
+                lastTween = DOTween.To(() => healthDamageFill.fillAmount, x => healthDamageFill.fillAmount = x, end, 1).SetDelay(0.2f).SetEase(Ease.InOutQuad, 0.1f);
+            }
+            else
+            {
+                healthDamageFill.fillAmount = start;
+                healthHealedFill.fillAmount = end;
+                healthFill.fillAmount = start;
+                lastSecondaryTween = DOTween.To(() => healthHealedFill.fillAmount, x => healthHealedFill.fillAmount = x, end, 0.15f).SetEase(Ease.InOutQuad, 0.1f);
+                lastTween = DOTween.To(() => healthFill.fillAmount, x => healthFill.fillAmount = x, end, 1).SetDelay(0.2f).SetEase(Ease.InOutQuad, 0.1f);
+            }
         }
+        
         private void OnHealthChange(object sender, EventParameters arg2)
         {
             UpdateMeter();
