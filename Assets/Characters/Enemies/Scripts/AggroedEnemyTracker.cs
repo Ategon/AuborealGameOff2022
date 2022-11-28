e149631e515961e4cca9caa639cc20944cb0d855
Leafcutter <alex.m.covington@gmail.com>
2022-11-28 17:54:51 -0500

    Added AggroedEnemyTracker to keep track of the number of aggroed enemies and music to a fighting version when it is greater than 0

---
+++ Assets/Characters/Enemies/Scripts/AggroedEnemyTracker.cs
@@ -0,0 +1,38 @@
+using Assets.Audio;
+using Assets.EventSystem;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Enemies
+{
+    [CreateAssetMenu(fileName = nameof(AggroedEnemyTracker), menuName = "ScriptableObjects/AggroedEnemyTracker")]
+    public class AggroedEnemyTracker : ScriptableObject
+    {
+        [SerializeField] private NumAggroedEnemyChangeEvent numAggroedEnemyChangeEvent;
+        [SerializeField] private AudioController audioController;
+        private int numEnemies;
+
+        private void OnEnable()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            numAggroedEnemyChangeEvent.AddListener(OnEnemyChangeEvent);
+        }
+        private void OnDisable()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+            numAggroedEnemyChangeEvent.RemoveListener(OnEnemyChangeEvent);
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+        {
+            numEnemies = 0;
+        }
+        private void OnEnemyChangeEvent(object sender, EventParameters arg2)
+        {
+            NumEnemyChangeEventParameters enemyChangeEventParameters = (NumEnemyChangeEventParameters)arg2;
+            numEnemies = Mathf.Max(0, enemyChangeEventParameters.changeValue + numEnemies);
+            audioController.SetFightParameter(numEnemies > 0);
+            Debug.Log(numEnemies);
+        }
+    }
+}
