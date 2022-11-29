84f2239ca6bb13bbeefbe0d166121f973a656b36
Benjamin Barbeau <benjamin@barbeau.net>
2022-11-29 06:08:53 -0500

    Add red viginette when hit

--- Assets/Characters/Player/Health/PlayerHealth.cs
+++ Assets/Characters/Player/Health/PlayerHealth.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Assets.Player.Health;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class PlayerHealth : Health
 {
@@ -10,13 +12,42 @@ public class PlayerHealth : Health
     [SerializeField] private float regenPeriod;
     private float timeToNextHeal;
 
+    private Volume v;
+    private Vignette vg;
+
+    private float redTime = 1f;
+    private float redTimer;
+
+    private float vgBaseIntensity;
+
     private void Awake()
     {
         timeToNextHeal = regenPeriod;
     }
 
+    private void Start()
+    {
+        v = FindObjectOfType<Volume>();
+        if(v) v.profile.TryGet(out vg);
+        if (vg) vgBaseIntensity = vg.intensity.value;
+    }
+
     void Update()
     {
+        if (redTimer > 0)
+        {
+            redTimer -= Time.deltaTime;
+            // change intensity based on sin
+            if (vg) vg.intensity.value = vgBaseIntensity + Mathf.Sin(Time.time * 20) / 10;
+            if (redTimer <= 0)
+            {
+                if (vg)
+                {
+                    vg.intensity.value = vgBaseIntensity;
+                    vg.color.value = Color.black;
+                }
+            }
+        }
         timeToNextHeal -= Time.deltaTime;
         if (timeToNextHeal <= 0)
         {
@@ -34,5 +65,11 @@ public class PlayerHealth : Health
     public override void ChangeHealth(int changeAmount)
     {
         healthController.ChangeHealth(changeAmount);
+
+        if (changeAmount < 0)
+        {
+            if(vg) vg.color.value = Color.red;
+            redTimer = redTime;
+        }
     }
 }
--- Assets/Tree.cs
+++ Assets/Tree.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Tree : MonoBehaviour
 {
+    private float offset;
+
+    private void Start()
+    {
+        offset = Random.Range(0f, 0.3f);
+    }
     private void FixedUpdate()
     {
         // rotate back and forth like a tree on a sin graph
-        transform.rotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.time) * 5);
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.time + offset) * 5);
         // also scale up and down like a tree on a sin graph
-        transform.localScale = Vector3.one * (Mathf.Sin(Time.time * 2 + 1) * 0.01f + 1);
+        transform.localScale = Vector3.one * (Mathf.Sin(Time.time * 2 + 1 + offset) * 0.01f + 1);
     }
 }
