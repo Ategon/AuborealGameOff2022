17ae49a9289fb14cf66ed637bd9368206b9e7622
NnYGames <NnYMakingGames@gmail.com>
2022-11-29 20:57:45 +0200

    Added details to island
    Added details like grass, creatures and creatures to the starter island. Had to change some of the trigger scripts to account for new collider rigid bodies (crabs and birds) in the scene

--- Assets/UI/Dialogue/DialogueTrigger.cs
+++ Assets/UI/Dialogue/DialogueTrigger.cs
@@ -5,10 +5,13 @@ public class DialogueTrigger : MonoBehaviour
 {
     [SerializeField] private Dialogue dialogue;
     [SerializeField] private string[] lines;
-    private void OnTriggerEnter2D()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        dialogue.gameObject.SetActive(true);
-        dialogue.StartDialogue(lines);
-        this.gameObject.SetActive(false);
+        if (collision.tag == "Player")
+        {
+            dialogue.gameObject.SetActive(true);
+            dialogue.StartDialogue(lines);
+            this.gameObject.SetActive(false);
+        }
     }
 }
